2c9847057de9740ed866f191acfdb92aea2baf9d
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2021-11-10 18:30:56 -0500

    changes on wdio config file

--- config/wdio.conf.ts
+++ config/wdio.conf.ts
@@ -26,7 +26,7 @@ exports.config = {
     specs: [
         // '.tsbuild/specs/**/*.js',
         // '.tsbuild/specs/robot.createFolder.e2e.js',
-        // '.tsbuild/specs/robot.interact.elements.js',
+        '.tsbuild/specs/robot.interact.elements.js',
         '.tsbuild/specs/the-internet.login.js'
 
     ],
@@ -61,7 +61,7 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        maxInstances: 1,
         //
         browserName: 'chrome',
         // If outputDir is provided WebdriverIO can capture driver session logs
