635131cbd2fe9c756cfb11de4b4326c9f73fa51b
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2021-11-17 22:15:40 -0500

    checking new pom file ok

--- src/test/specs/robot.interact.elements.dropdown.test.ts
+++ src/test/specs/robot.interact.elements.dropdown.test.ts
@@ -8,19 +8,44 @@ describe('Interact with elements on the web page', () => {
         FillRoboForm.getElemetFromList();
     });
 
-    it('should clear a value', () => {
+    it('should Take a screenshot', () => {
         browser.takeScreenshot();
         allureReporter.addSeverity('critical')
-        // FillRoboForm.checkClearValues('Doctor');
+    });
+
+    it('should be able to setup a text', () => {
         FillRoboForm.setTitleRoboForm('Engineer');
+    });
+
+    it('should be able to clear a value', () => {
         FillRoboForm.clearTitleValue();
+    });
+
+    it('should be able to do a click on the web page', () => {
         FillRoboForm.clickFullName();
+    });
+
+    it('should be able to check if a element is displayed', () => {
         FillRoboForm.checkIfBuyNowButtonIsDisplayed();
+    });
+
+    it('should be able to clear a value', () => {
+        FillRoboForm.checkIfBuyNowButtonIsDisplayed();
+    });
+
+    it('should be able to choose any element from a list', () => {
         FillRoboForm.chooseCarExpirationDateMonth('12');
         FillRoboForm.selectCreditCardType('American Express');
         driver.pause(3000);
+    });
+
+    it('should be able to do scroll to search any element', () => {
         FillRoboForm.scrollEnglishButton();
         driver.pause(3000);
+        FillRoboForm.waitUntilTheEnglishButtonIsDisplayed();
+    });
+
+    it('should be able to do wait until any element is visible', () => {
         FillRoboForm.waitUntilTheEnglishButtonIsDisplayed();
     });
 })
